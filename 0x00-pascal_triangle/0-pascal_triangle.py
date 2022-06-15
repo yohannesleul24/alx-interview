8b5e16056a259fb3c06fb46076708db024f7ce30
yohannesleul24 <yohannesleul24@gmail.com>
2022-06-15 09:43:42 +0000

    interview

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Pascal Triangle """
+
+
+def pascal_triangle(n):
+    """calculate pascal, return list"""
+    pascal_list = []
+
+    if n <= 0:
+        return pascal_list
+
+    for i in range(n):
+        temp_list = []
+        for j in range(i + 1):
+            if (j == 0 or j == i):
+                temp_list.append(1)
+            else:
+                temp_list.append(pascal_list[i-1][j-1] + pascal_list[i-1][j])
+        pascal_list.append(temp_list)
+    return pascal_list
